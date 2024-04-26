be6d169d48a87be010ddaf6cc64ebbd198a7c4f3
Jack <jack.nguyen@techvify.com.vn>
2024-04-26 17:25:06 +0700

    update: modifiable graph now available; TODO separate mode by roles.

--- src/routes/learn_routes.py
+++ src/routes/learn_routes.py
@@ -1,5 +1,6 @@
 """Routes to support teaching through our website.
 Should also helps with proper implementation of roles (Teacher / Student should have their own accessibles)."""
+import io
 import flask
 from flask import Flask, request, url_for
 
@@ -15,17 +16,29 @@ def build_learn_routes(app: Flask, login_decorator: callable=lambda f: f) -> Fla
     @app.route("/self_learn")
     @login_decorator
     def self_learn():
-        return flask.render_template("self_learn.html")
+        return flask.render_template("self_learn.html", can_model=True)
 
     @app.route("/self_learn_upload", methods=["POST"])
     def self_learn_upload():
         # allow uploading the saved xml from bpmn into local storage.
         try:
-            file = request.files["file"]
-            file.save(DEFAULT_LEARN_XML_POSITION)
+            content = request.data#.files["file"]
+            with io.open(DEFAULT_LEARN_XML_POSITION, "w") as wf:
+                wf.write(content.decode("utf-8"))
+#            file.save(DEFAULT_LEARN_XML_POSITION)
         except Exception as e:
             return flask.jsonify(result=False, error=str(e))
         return flask.jsonify(result=True, uploaded_location=DEFAULT_LEARN_XML_POSITION)
+
+    @app.route("/self_learn_download", methods=["GET"])
+    def self_learn_download():
+        # allow downloading the content of the saved xml 
+        try: 
+            with io.open(DEFAULT_LEARN_XML_POSITION, "r") as rf:
+                data = rf.read()
+            return flask.jsonify(result=True, data=data)
+        except Exception as e:
+            return flask.jsonify(result=False, error=str(e))
 
     return app
 
---
+++ static/self_learn.js
@@ -0,0 +1,53 @@
+var bpmnViewer = new BpmnJS({container: "#bpmn_canvas"});
+
+async function load_graph_fron_xml(xml_content_or_json) {
+	let xml_content = (typeof xml_content_or_json === 'string' || xml_content_or_json instanceof String) ? xml_content_or_json : xml_content_or_json.data;
+	// console.log(xml_content);
+	try {
+		await bpmnViewer.importXML(xml_content);
+		// access viewer components
+		var canvas = bpmnViewer.get('canvas');
+		var overlays = bpmnViewer.get('overlays');
+		// zoom to fit full viewport
+		canvas.zoom('fit-viewport');
+	} catch (err) {
+		console.error('could not import BPMN 2.0 diagram', err);
+	}
+}
+
+
+var test_graph = 'https://cdn.statically.io/gh/bpmn-io/bpmn-js-examples/dfceecba/starter/diagram.bpmn';
+function open_graph_from_online_link(event, link=test_graph, mode="text") {
+	$.get(link, load_graph_fron_xml, mode);
+}
+
+function open_graph_from_server(event) {
+	return open_graph_from_online_link(event, "self_learn_download", "json");
+}
+
+function export_graph(event, graph_name="graph.bpmn") {
+	// just console.log for now 
+	bpmnViewer.saveXML({ format: true }).then(function(result) {
+		console.log(result.xml)
+		const blob = new Blob([result.xml], {type: 'text/plain'});
+		let url = window.URL.createObjectURL(blob);
+		let link = $("#hidden_xml_link");
+		link.attr("href", url).attr("download", graph_name);
+		link[0].click();
+	});
+	console.log("Export should be done now");
+}
+
+function upload_graph_into_server(event, server_location="") {
+	// guess I can just push the entire thing as string; what's the harm?
+	bpmnViewer.saveXML({ format: true}).then(function(result) {
+		$.post({
+			url: "self_learn_upload",
+			data: result.xml,
+			contentType: "text/html",
+			dataType: "html"
+		}, function(data, status) {
+			console.log("Upload done", data, status);
+		})
+	});
+}
